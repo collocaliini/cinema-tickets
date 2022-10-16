a7a13a8db35955e02b561cd0843939da348ab071
collocaliini <68927740+collocaliini@users.noreply.github.com>
2022-10-16 17:23:01 +0100

    Convert TicketTypeRequest to immutable dataclass

--- cinema-tickets-python/ticket_type_request.py
+++ cinema-tickets-python/ticket_type_request.py
@@ -1,31 +1,29 @@
+from dataclasses import dataclass
 
+
+VALID_TICKET_TYPES = ("ADULT", "CHILD", "INFANT")
+
+
+@dataclass(frozen=True)
 class TicketTypeRequest:
+    ticket_type: str
+    number_of_tickets: int
 
-    _valid_types = ("ADULT", "CHILD", "INFANT")
-
-    def __init__(self, ticket_type, number_of_tickets):
-
-        if not isinstance(ticket_type, str):
-
+    def __post_init__(self):
+        if not isinstance(self.ticket_type, str):
             raise TypeError("ticket_type must be a string")
 
-        if not ticket_type in self._valid_types:
+        if self.ticket_type not in VALID_TICKET_TYPES:
+            raise TypeError(f"ticket_type must be one of: {','.join(VALID_TICKET_TYPES)}")
 
-            raise TypeError(
-                f"ticket_type must be one of: {','.join(self._valid_types)}")
-
-        if not isinstance(number_of_tickets, int):
-
+        if not isinstance(self.number_of_tickets, int):
             raise TypeError("number_of_tickets must be an integer")
 
-        self.ticket_type = ticket_type
-
-        self.number_of_tickets = number_of_tickets
+        if not self.number_of_tickets >= 0:
+            raise ValueError("number_of_tickets must be positive")
 
     def get_ticket_type(self):
-
         return self.ticket_type
 
     def get_tickets_number(self):
-
         return self.number_of_tickets
